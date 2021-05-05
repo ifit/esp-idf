439720d22bcdde4e33fd7997a8fff6c0745414b9
Ben Byers <ben.byers@iconfitness.com>
2021-05-05 15:45:18 -0600

    Update toolchain-esp32.cmake to use our updated version

--- tools/cmake/toolchain-esp32.cmake
+++ tools/cmake/toolchain-esp32.cmake
@@ -1,8 +1,42 @@
-set(CMAKE_SYSTEM_NAME Generic)
+message(STATUS "importing the toolchain cmake directory")
 
-set(CMAKE_C_COMPILER xtensa-esp32-elf-gcc)
-set(CMAKE_CXX_COMPILER xtensa-esp32-elf-g++)
-set(CMAKE_ASM_COMPILER xtensa-esp32-elf-gcc)
+if (CMAKE_HOST_SYSTEM_NAME STREQUAL "Darwin")
+    get_filename_component( WORK_PATH "${ICON_TOOLCHAIN_DIR}/mac/xtensa-esp32-elf/bin" ABSOLUTE)
+    #Set the ESP device port if it has not been set before
+    if(DEFINED ESPPORT_OVER_RIDE)
+        set(ESPPORT ${ESPPORT_OVER_RIDE})
+    else()
+        set(ESPPORT /dev/cu.SLAB_USBtoUART)
+    endif(DEFINED ESPPORT_OVER_RIDE)
+    set(ESPBAUD 115200)
+    set(ESPRESSIF_TOOLCHAIN_PATH "${WORK_PATH}")
+	set(OPENOCD_PATH "${ICON_TOOLCHAIN_DIR}/mac/openocd-esp32")
+elseif (CMAKE_HOST_SYSTEM_NAME STREQUAL "Linux")
+    get_filename_component( WORK_PATH "${ICON_TOOLCHAIN_DIR}/linux/xtensa-esp32-elf/bin" ABSOLUTE)
+    #Set the ESP device port if it has not been set before
+    if(DEFINED ESPPORT_OVER_RIDE)
+        set(ESPPORT ${ESPPORT_OVER_RIDE})
+    else()
+        set(ESPPORT /dev/ttyUSB0)
+    endif(DEFINED ESPPORT_OVER_RIDE)
+    set(ESPBAUD 115200)
+    set(ESPRESSIF_TOOLCHAIN_PATH "${WORK_PATH}")
+	set(OPENOCD_PATH "${ICON_TOOLCHAIN_DIR}/linux/openocd-esp32")
+else()
+    message( FATAL_ERROR "Cannot Configure for ${CMAKE_HOST_SYSTEM_NAME}")
+endif()
+
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_C_COMPILER ${WORK_PATH}/xtensa-esp32-elf-gcc)
+set(CMAKE_CXX_COMPILER ${WORK_PATH}/xtensa-esp32-elf-g++)
+set(CMAKE_ASM_COMPILER ${WORK_PATH}/xtensa-esp32-elf-gcc)
+set(CMAKE_GDB ${WORK_PATH}/xtensa-esp32-elf-gdb)
+set(CMAKE_OBJDUMP ${WORK_PATH}/xtensa-esp32-elf-objdump)
 
 set(CMAKE_C_FLAGS "-mlongcalls -Wno-frame-address" CACHE STRING "C Compiler Base Flags")
 set(CMAKE_CXX_FLAGS "-mlongcalls -Wno-frame-address" CACHE STRING "C++ Compiler Base Flags")
+
+# Can be removed after gcc 5.2.0 support is removed (ref GCC_NOT_5_2_0)
+set(CMAKE_EXE_LINKER_FLAGS "-nostdlib" CACHE STRING "Linker Base Flags")
+
+
